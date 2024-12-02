84260473eec05984a2d415d4b440e8b8a0c0f6eb
Victor Botez <botez.victor.a@gmail.com>
2024-12-02 18:40:54 +0100

    switch to new branch

--- job_launcher/job_pod_computation.sh
+++ job_launcher/job_pod_computation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-#SBATCH --job-name=pod
+#SBATCH --job-name=pod_hydro
 #SBATCH -o ./JobLogs/%x.o
 #SBATCH -e ./JobLogs/%x.err
 #SBATCH --exclusive
-#SBATCH --ntasks=50
-#SBATCH --partition=mem_short
-#SBATCH --time=1:00:00
+#SBATCH --ntasks=100
+#SBATCH --partition=mem
+#SBATCH --time=72:00:00
 
 
 cd ${SLURM_SUBMIT_DIR}
@@ -20,7 +20,7 @@ source /gpfs/users/botezv/.venv/pod/bin/activate
 
 set -x
 
-data_directory="/gpfs/users/botezv/APPLICATIONS_POD/pod_computations/job_launcher/data_test.txt"
+data_directory="/gpfs/users/botezv/APPLICATIONS_POD/pod_computations/job_launcher/data_hydro.txt"
 
 echo 'running pod'
 # srun python /gpfs/users/botezv/APPLICATIONS_POD/pod_computations_test_angle_shift_v3/initialization.py "$data_directory"
