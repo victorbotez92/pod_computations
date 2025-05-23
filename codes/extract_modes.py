9292ae333b0a067a2fafd4539e20f6abff65505e
Victor Botez <botez.victor.a@gmail.com>
2025-05-23 17:30:47 +0200

    validated POD modes for m-families without crossed-correlations

--- codes/extract_modes.py
+++ codes/extract_modes.py
@@ -19,13 +19,23 @@ def main_extract_modes(par):
     #         energies = np.transpose(np.loadtxt(f))
     #     nb_DR = int(field[3])
     #     magnetic_energies = energies[20*nb_DR+10,:]
-    if par.should_we_save_phys_POD:
-        a_phys = np.load(par.complete_output_path+par.output_file_name+"/a_phys_(mode_time).npy") # signature n,T
-        Nt = a_phys.shape[-1]
-        if par.should_we_save_all_phys_pod_modes:
-            par.phys_pod_modes_to_save = np.arange(Nt)
-        a_phys = a_phys[par.phys_pod_modes_to_save]
-        e_phys = np.square(a_phys).sum(-1)/Nt
+
+
+    # if par.should_we_save_phys_POD:
+        # list_a_phys = []
+        # for new_family in list_m_families:
+        #     m = new_family.min
+        #     list_a_phys.append(np.load(par.complete_output_path+par.output_file_name+f"/a_phys_(mode_time)_m{m}.npy"))
+        # list_a_phys = np.array(list_a_phys)
+        # a_phys = np.load(par.complete_output_path+par.output_file_name+"/a_phys_(mode_time).npy") # signature n,T
+        # Nt = a_phys.shape[-1]
+        # if par.should_we_save_all_phys_pod_modes:
+            # par.phys_pod_modes_to_save = np.arange(Nt)
+        # list_a_phys = list_a_phys[:, par.phys_pod_modes_to_save]
+        # a_phys = a_phys[par.phys_pod_modes_to_save]
+        # list_e_phys = np.square(list_a_phys).sum(-1)/Nt
+        # e_phys = np.square(a_phys).sum(-1)/Nt
+        # list_phys_pod_modes = None
 
 
     if par.should_we_save_Fourier_POD and par.should_we_save_all_fourier_pod_modes:
@@ -33,10 +43,24 @@ def main_extract_modes(par):
         Nt = a_fourier.shape[-1]
         par.fourier_pod_modes_to_save = np.arange(Nt)
 
-    for i,mF in enumerate(par.list_modes[par.rank_fourier::par.nb_proc_in_fourier]):
+    for num_mF,mF in enumerate(par.list_modes[par.rank_fourier::par.nb_proc_in_fourier]):
     # for mF in range(par.rank_fourier,par.MF,par.nb_proc_in_fourier):
+        if par.should_we_save_phys_POD:
+            bool_found = False
+            index_phys_mode = 0
+            while not bool_found:
+                if mF in par.list_m_families[index_phys_mode]:
+                    bool_found = True
+                else:
+                    index_phys_mode += 1
+            fourier_family = par.list_m_families[index_phys_mode].min()
+            a_phys = np.load(par.complete_output_path+par.output_file_name+f"/a_phys_(mode_time)_m{fourier_family}.npy") # signature n,T
+            Nt = a_phys.shape[-1]
+            e_phys = np.square(a_phys).sum(-1)/Nt
+            phys_pod_modes = None
+
         if par.rank == 0:
-            write_job_output(par.path_to_job_output,f'entering Fourier loop {i//par.nb_proc_in_fourier+1}/{len(par.list_modes[par.rank_fourier::par.nb_proc_in_fourier])//par.nb_proc_in_fourier}')
+            write_job_output(par.path_to_job_output,f'entering Fourier loop {num_mF//par.nb_proc_in_fourier+1}/{len(par.list_modes[par.rank_fourier::par.nb_proc_in_fourier])//par.nb_proc_in_fourier}')
         list_axis = ["c","s"]
         for a in range(par.rank_axis,2,par.nb_proc_in_axis):
             axis = list_axis[a]
@@ -63,11 +87,17 @@ def main_extract_modes(par):
                         write_job_output(par.path_to_job_output,f'  Importing {path_to_data}')
                     new_data = import_data(par,mF,axis,path_to_data,par.field_name_in_file) #shape t (d n) [with a being only shape 1]
 
+                    if phys_pod_modes is None:
+                        phys_pod_modes = np.zeros((a_phys.shape[0], new_data.shape[-1]))
+                    # if list_phys_pod_modes is None:
+                        # list_phys_pod_modes = [np.zeros((list_a_phys[0].shape[0], new_data.shape[-1])) for _ in list_m_families]
+
                     if i == 0:
                         local_nb_snapshots,previous_nb_snapshots = new_data.shape[0],0
                         if par.should_we_save_Fourier_POD:
                             fourier_pod_modes = 1/(Nt*e_fourier[:,None]) * a_fourier[:,:local_nb_snapshots]@new_data
                         if par.should_we_save_phys_POD:
+                            # list_phys_pod_modes[index_phys_mode] = 1/(Nt*list_e_phys[index_phys_mode, :,None]) * list_a_phys[index_phys_mode, :, :local_nb_snapshots]@new_data
                             phys_pod_modes = 1/(Nt*e_phys[:,None]) * a_phys[:,:local_nb_snapshots]@new_data
 
                     else:
@@ -75,6 +105,7 @@ def main_extract_modes(par):
                         if par.should_we_save_Fourier_POD:
                             fourier_pod_modes += 1/(Nt*e_fourier[:,None]) * a_fourier[:,previous_nb_snapshots:local_nb_snapshots]@new_data
                         if par.should_we_save_phys_POD:
+                            # list_phys_pod_modes[index_phys_mode] += 1/(Nt*list_e_phys[index_phys_mode, :,None]) * list_a_phys[index_phys_mode, :, :local_nb_snapshots]@new_data
                             phys_pod_modes += 1/(Nt*e_phys[:,None]) * a_phys[:,previous_nb_snapshots:local_nb_snapshots]@new_data
                 ############## Adding the symmetrized part when asked THIS ONLY WORKS FOR RPI SYMMETRY
                     if par.should_we_add_mesh_symmetry:
@@ -96,6 +127,7 @@ def main_extract_modes(par):
                         if par.should_we_save_Fourier_POD:
                             fourier_pod_modes += 1/(Nt*e_fourier[:,None]) * a_fourier[:,Nt//2+previous_nb_snapshots:Nt//2+local_nb_snapshots]@sym_data
                         if par.should_we_save_phys_POD:
+                            # list_phys_pod_modes[index_phys_mode] += 1/(Nt*list_e_phys[index_phys_mode, :,None]) * list_a_phys[index_phys_mode, :,Nt//2+previous_nb_snapshots:Nt//2+local_nb_snapshots]@sym_data
                             phys_pod_modes += 1/(Nt*e_phys[:,None]) * a_phys[:,Nt//2+previous_nb_snapshots:Nt//2+local_nb_snapshots]@sym_data
                         del sym_data
                         gc.collect()
@@ -108,6 +140,6 @@ def main_extract_modes(par):
                 if par.should_we_save_phys_POD:
                     for m_i in range(par.phys_pod_modes_to_save.size):
                         nP=par.phys_pod_modes_to_save[m_i]
-                        np.save(par.complete_output_path+par.output_file_name+f"/phys_pod_modes/nP_{nP:03d}_mF_{mF:03d}_{axis}",phys_pod_modes[m_i])
+                        np.save(par.complete_output_path+par.output_file_name+f"/phys_pod_modes/m_{fourier_family}_nP_{nP:03d}_mF_{mF:03d}_{axis}",phys_pod_modes[m_i])
         # end for axis in [cos,sin]
     # end for mF in MF
